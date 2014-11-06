32592d9dc55364f1ac3b6916ac9da3f75296da3e
Brian Hays <brian.hays@gmail.com>
2014-11-06 16:52:34 -0500

    Change OSX version test

--- setup_rails.sh
+++ setup_rails.sh
@@ -3,6 +3,12 @@
 # This is the SetupRails script!
 # 
 # author: Brian Hays
+# website: (coming soon)
+#
+# If you're viewing this in your web browser, and want to install Ruby on Rails...
+# Open up your terminal and type:
+#
+#    curl (installer site coming soon)
 #
 # SetupRails currently supports:
 #   - Mac: OS X 10.9 and above
@@ -19,11 +25,7 @@ setup_rails () {
 
 ### Now, on to the actual installer...
 
-### NOTE sh NOT bash. This script should be POSIX sh only, since we don't
-### know what shell the user has. Debian uses 'dash' for 'sh', for
-### example.
-
-PREFIX="/usr/local"
+### NOTE sh NOT bash. This script should be POSIX sh
 
 set -e
 set -u
@@ -41,15 +43,18 @@ fi
 
 if [ "$UNAME" = "Darwin" ] ; then
   ### OSX ###
-  OSX_VERSION=$(sw_vers -productVersion)
-  if [ "$OSX_VERSION" != "10.10" -a "$OSX_VERSION" != "10.9" ] ; then
+  OSX_VERSION=$(sw_vers -productVersion | awk 'BEGIN{FS=".";} {print $2}' )
+  if [ "$OSX_VERSION" != "10" -a "$OSX_VERSION" != "9" ] ; then
     echo "Only OSX Yosemite and Mavericks are supported at this time."
     exit 1
   fi
 fi
 
+### echo for TESTING ONLY ###
 echo "$UNAME"
 echo "$OSX_VERSION"
+#############################
+
 trap - EXIT
 }
 
